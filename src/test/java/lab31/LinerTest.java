62e58bd0d1e946d1aa6853a5dccffb5fb7faa0b6
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2018-12-06 23:05:22 +0200

    add test lab31

--- src/test/java/lab31/LinerTest.java
+++ src/test/java/lab31/LinerTest.java
@@ -25,5 +25,23 @@ public class LinerTest{
     public void check_successful_remove_ship(){
 
         AbstractShip testLiner = new Liner("NameTestLiner",1000, 1000, 1000, 100);
+        AbstractShip testCargo = new Cargo("NameTestCargo", 1000, 1000, 1000,100);
+        AbstractShip testTanker = new Tanker("NameTestTanker", 1000, 1000, 1000, 100);
+
+        OdessaSeaPort odessaSeaPort = new OdessaSeaPort();
+
+        odessaSeaPort.addShipToEndQueue(testCargo);
+        odessaSeaPort.addShipToEndQueue(testLiner);
+        odessaSeaPort.addShipToEndQueue(testTanker);
+
+        int expectedSuccessfulStatusRemoveShipInQueue = 1;
+        assertEquals(expectedSuccessfulStatusRemoveShipInQueue, odessaSeaPort.removeShipFromBeginQueue());
+
+        String expectedResultPrintShipQueueAfterRemove =
+                "{Name=NameTestLinerLength=1000.0Width=1000.0Displacement=1000.0};" +
+                "{Name=NameTestTankerLength=1000.0Width=1000.0Displacement=1000.0};";
+
+        assertEquals(expectedResultPrintShipQueueAfterRemove, odessaSeaPort.printQueueShip());
+
     }
 }
