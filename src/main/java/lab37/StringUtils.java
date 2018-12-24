ac62404445279edc2246566f4cfe1c11a555140d
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2018-12-25 01:00:59 +0200

    solved lab 37 and fixed method for testing on juja

--- src/main/java/lab37/StringUtils.java
+++ src/main/java/lab37/StringUtils.java
@@ -2,6 +2,9 @@ package lab37;
 
 public class StringUtils {
     public static String rightShift(String arg, int delta) {
+        if (arg == null || arg.length() < 2){
+            return arg;
+        }
         while (delta < 0) {
             delta += arg.length();
         }
